e4251a3add098764b2b738649ecb74e971686c7f
Mostafa <mosi3883@gmail.com>
2022-06-04 16:43:59 +0430

    pagination logic

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,15 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  const itemsPerPage = 8;
+  const maxPages = Math.ceil(followers.length / itemsPerPage);
 
-export default paginate
+  const newFollowers = [];
+
+  for (let i = 0; i < maxPages; i++) {
+    const start = i * itemsPerPage;
+    const end = start + itemsPerPage;
+    newFollowers.push(followers.slice(start, end));
+  }
+  return newFollowers;
+};
+
+export default paginate;
